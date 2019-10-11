f3e2ffd51538787db656af22b8f66db141245807
Max Rozdobudko <max.rozdobudko@gmail.com>
2019-10-11 15:08:46 +0300

    Creates Xcode project.

---
+++ Resto/Resto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resto
+//
+//  Created by Max Rozdobudko on 10/11/19.
+//  Copyright © 2019 Max Rozdobudko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
